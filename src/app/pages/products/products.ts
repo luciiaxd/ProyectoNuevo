d70d3bb6659b1467b2caef9e39cbbed7d109b752
luciiaxd <luciabgxdd@gmail.com>
2025-10-23 16:55:22 -0300

    le puse nombres gg

--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -16,64 +16,64 @@ public infoTarjeta: Tarjeta [];
         id: "1T",
         img: "../../../assets/5ede827ef2414-miss-dior-dama-jpg.jpg",
         alt: "Imagen de un perfume",
-        titulo: "Perfume",
-        descripcion: "Un aroma cautivador",
+        titulo: "Miss Dior",
+        descripcion: "Oriental Floral",
         fechapublicacion: 2025
       },
       {
         id: "2T",
         img: "../../../assets/chanel.jpg",
         alt: "Imagen de un perfume",
-        titulo: "Perfume",
-        descripcion: "Un aroma cautivador",
+        titulo: "Chanel N°5",
+        descripcion: "Aldehídos, ylang-ylang, neroli, bergamota y limón.",
         fechapublicacion: 2025
       },
       {
         id: "3T",
         img: "../../../assets/perfume mujer dolce gabbana,jpg.jpg",
         alt: "Imagen de un perfume",
-        titulo: "Perfume",
-        descripcion: "Un aroma cautivador",
+        titulo: "The one Dolce&Gabbana",
+        descripcion: "dulce floral",
         fechapublicacion: 2025
       },
       {
         id: "4T",
         img: "../../../assets/coso.jpg",
         alt: "Imagen de un perfume",
-        titulo: "Perfume",
-        descripcion: "Un aroma cautivador",
+        titulo: "Beach Walk Maison Margiela",
+        descripcion: "bergamota e ylang-ylang",
         fechapublicacion: 2025
       },
       {
         id: "5T",
         img: "../../../assets/cloud.jpg",
         alt: "Imagen de un perfume",
-        titulo: "Perfume",
-        descripcion: "Un aroma cautivador",
+        titulo: "Cloud Ariana Grande",
+        descripcion: " floral frutal gourmand",
         fechapublicacion: 2025
       },
       {
         id: "6T",
         img: "../../../assets/cloudpink.jpg",
         alt: "Imagen de un perfume",
-        titulo: "Perfume",
-        descripcion: "Un aroma cautivador",
+        titulo: "Cloud Pink Ariana Grande",
+        descripcion: "Oriental Vainilla",
         fechapublicacion: 2025
       },
       {
         id: "7T",
         img: "../../../assets/tnknext.jpg",
         alt: "aa",
-        titulo:"aa",
-        descripcion: "aa",
+        titulo:"Thank U, Next Ariana Grande",
+        descripcion: " floral frutal gourmand",
         fechapublicacion:0
       },
       {
         id: "8T",
         img: "../../../assets/arianag.jpg",
         alt: "Imagen de un perfume",
-        titulo: "Perfume",
-        descripcion: "Un aroma cautivador",
+        titulo: "Ari Ariana Grande",
+        descripcion: "Aroma floral frutal.",
         fechapublicacion: 2025
       }
       
